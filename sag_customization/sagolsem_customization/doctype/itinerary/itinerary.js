b017481f7665696cc1d09a4a41193fd24a21ef29
Sun Howwrongbum <sun@libermatic.com>
2018-08-13 01:46:07 +0530

    Itinerary: handle totals and gl entries

--- sag_customization/sagolsem_customization/doctype/itinerary/itinerary.js
+++ sag_customization/sagolsem_customization/doctype/itinerary/itinerary.js
@@ -2,7 +2,73 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Itinerary', {
-	refresh: function(frm) {
-
-	}
+  onload: async function(frm) {
+    frm.fields_dict['expense_account'].get_query = doc => ({
+      filters: {
+        root_type: 'Expense',
+        is_group: false,
+      },
+    });
+    frm.fields_dict['income_account'].get_query = doc => ({
+      filters: {
+        root_type: 'Income',
+        is_group: false,
+      },
+    });
+    const { message: accounts = {} } = await frappe.db.get_value(
+      'Itinerary Settings',
+      null,
+      ['expense_account', 'income_account']
+    );
+    frm.set_value('expense_account', accounts['expense_account']);
+    frm.set_value('income_account', accounts['income_account']);
+  },
+  refresh: function(frm) {
+    frappe.ui.form.on('Itinerary Charge', {
+      charge_name: async function(frm, cdt, cdn) {
+        const { charge_name } = locals[cdt][cdn] || {};
+        if (charge_name) {
+          const { message: charge } = await frappe.db.get_value(
+            'Itinerary Charge Type',
+            charge_name,
+            'default_value'
+          );
+          frappe.model.set_value(cdt, cdn, 'amount', charge['default_value']);
+        }
+      },
+      amount: function(frm) {
+        frm.trigger('calculate_totals');
+      },
+      payments_remove: function(frm) {
+        frm.trigger('calculate_totals');
+      },
+    });
+  },
+  before_save: function(frm) {
+    frm.trigger('calculate_totals');
+  },
+  mode_of_payment: async function(frm) {
+    const { mode_of_payment, company } = frm.doc;
+    frm.toggle_reqd(['cheque_no', 'cheque_date'], mode_of_payment === 'Cheque');
+    frm.toggle_display(
+      ['cheque_no', 'cheque_date'],
+      mode_of_payment === 'Cheque'
+    );
+    const { message } = await frappe.call({
+      method:
+        'erpnext.accounts.doctype.sales_invoice.sales_invoice.get_bank_cash_account',
+      args: { mode_of_payment, company },
+    });
+    if (message) {
+      frm.set_value('payment_account', message.account);
+    }
+  },
+  calculate_totals: function(frm) {
+    const { payments = [] } = frm.doc;
+    const total_charges = payments.reduce((a, { amount: x = 0 }) => a + x, 0);
+    const rounded_total = flt(total_charges, 0);
+    frm.set_value('total', total_charges);
+    frm.set_value('rounding_adjustment', rounded_total - total_charges);
+    frm.set_value('rounded_total', rounded_total);
+  },
 });
--- sag_customization/sagolsem_customization/doctype/itinerary/itinerary.py
+++ sag_customization/sagolsem_customization/doctype/itinerary/itinerary.py
@@ -4,7 +4,52 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.model.document import Document
+from frappe.utils import money_in_words
+from erpnext.controllers.accounts_controller import AccountsController
+from erpnext.accounts.general_ledger import make_gl_entries
 
-class Itinerary(Document):
-	pass
+
+class Itinerary(AccountsController):
+    def before_save(self):
+        if self.mode_of_payment != 'Cheque':
+            self.cheque_no = None
+            self.cheque_date = None
+        currency = frappe.db.get_value(
+            'Company', self.company, 'default_currency'
+        )
+        self.in_words = money_in_words(self.rounded_total, currency)
+
+    def on_submit(self):
+        self.make_gl_entries()
+
+    def on_cancel(self):
+        self.make_gl_entries(cancel=1)
+
+    def make_gl_entries(self, cancel=0, adv_adj=0):
+        cost_center, round_off_account = frappe.db.get_value(
+            'Company',
+            self.company,
+            ['cost_center', 'round_off_account'],
+        )
+        gl_entries = [
+            self.get_gl_dict({
+                'account': self.income_account,
+                'credit': self.total,
+                'cost_center': cost_center,
+            }),
+            self.get_gl_dict({
+                'account': self.payment_account,
+                'debit': self.rounded_total,
+            }),
+        ]
+        if self.rounding_adjustment:
+            gl_entries.append(
+                self.get_gl_dict({
+                    'account': round_off_account,
+                    'credit': self.rounding_adjustment,
+                    'cost_center': cost_center,
+                }),
+            )
+        make_gl_entries(
+            gl_entries, cancel=cancel, adv_adj=adv_adj, merge_entries=False
+        )
