888cccb7dde738af1bb67e4ee7ee8f02961d99d6
Sun Howwrongbum <sun@libermatic.com>
2018-08-13 01:34:49 +0530

    Itinerary Settings: add expense account

--- sag_customization/sagolsem_customization/doctype/itinerary_settings/itinerary_settings.js
+++ sag_customization/sagolsem_customization/doctype/itinerary_settings/itinerary_settings.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Itinerary Settings', {
-	refresh: function(frm) {
-
-	}
+  refresh: function(frm) {
+    frm.fields_dict['expense_account'].get_query = doc => ({
+      filters: {
+        root_type: 'Expense',
+        is_group: false,
+      },
+    });
+    frm.fields_dict['income_account'].get_query = doc => ({
+      filters: {
+        root_type: 'Income',
+        is_group: false,
+      },
+    });
+  },
 });
